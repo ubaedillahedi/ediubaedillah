6c6d6f8cae62cffa6fc34f65d29623f68ce44e87
ediubaedillah <ubaedillahedi@gmail.com>
2019-03-27 18:44:36 +0700

    Updates

---
+++ precache-manifest.fecf9d36ae4c7fa2d0c0e669b18aa5d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ediubaedillah/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b16961385059ee8d4208",
+    "url": "/ediubaedillah/static/js/runtime~main.c3a6f89a.js"
+  },
+  {
+    "revision": "d03e07a21d8996567115",
+    "url": "/ediubaedillah/static/js/main.63d1df86.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/ediubaedillah/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "d03e07a21d8996567115",
+    "url": "/ediubaedillah/static/css/main.1871a3ec.chunk.css"
+  },
+  {
+    "revision": "f10c8400cf0776ccde3736b193f9c6fb",
+    "url": "/ediubaedillah/index.html"
+  }
+];
